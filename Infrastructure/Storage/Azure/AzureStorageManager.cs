34f52f6c372aa344815639830fa5b11cc5cc2ed1
Baris Kortun <bariskorun@gmail.com>
2023-02-28 11:54:42 +0300

    Azure storage was added

--- Infrastructure/Storage/Azure/AzureStorageManager.cs
+++ Infrastructure/Storage/Azure/AzureStorageManager.cs
@@ -1,5 +1,8 @@
 ﻿using Application.Services.Storage.Azure;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,26 +11,48 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Storage.Azure
 {
-    public class AzureStorageManager : IAzureStorageService
+    public class AzureStorageManager :Storage, IAzureStorageService
     {
-        public Task DeleteFileAsync(string pathOrContainerName, string fileName)
+        readonly BlobServiceClient _blobServiceClient;
+        BlobContainerClient _blobContainerClient;
+        public AzureStorageManager(IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _blobServiceClient = new(configuration["Storage:Azure"]);
+        }
+        public async Task DeleteFileAsync(string containerName, string fileName)
+        {
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            BlobClient blobClient = _blobContainerClient.GetBlobClient(fileName);
+            await blobClient.DeleteAsync();
         }
 
-        public List<string> GetFilesName(string pathOrContainerName)
+        public List<string> GetFilesName(string containerName)
         {
-            throw new NotImplementedException();
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            return _blobContainerClient.GetBlobs().Select(blob => blob.Name).ToList();
         }
 
-        public bool HasFile(string pathOrContainerName, string fileName)
+        public bool HasFile(string containerName, string fileName)
         {
-            throw new NotImplementedException();
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            return _blobContainerClient.GetBlobs().Any(blob => blob.Name == fileName);
         }
 
-        public Task<List<Domain.Entities.File>> UploadAsync(string pathOrContainerName, IFormFileCollection formFiles)
+        public async Task<List<Domain.Entities.File>> UploadAsync(string containerName, IFormFileCollection formFiles)
         {
-            throw new NotImplementedException();
+            _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            await _blobContainerClient.CreateIfNotExistsAsync();
+            await _blobContainerClient.SetAccessPolicyAsync(PublicAccessType.BlobContainer);
+
+            List<Domain.Entities.File> datas = new();
+            foreach (IFormFile file in formFiles)
+            {
+                string fileNewName = await FileRenameAsync(file.FileName);
+                BlobClient blobClient = _blobContainerClient.GetBlobClient(fileNewName);
+                await blobClient.UploadAsync(file.OpenReadStream());
+                datas.Add(new() {Name= fileNewName, Path= $"{containerName}/{fileNewName}"});
+            }
+            return datas;
         }
     }
 }
